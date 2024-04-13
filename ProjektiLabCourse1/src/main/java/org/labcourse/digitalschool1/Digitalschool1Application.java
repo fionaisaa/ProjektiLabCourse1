1f32c6b246dc9e4a9097f57b9d22f8ec07620d2d
fionaisaa <157630752+fionaisaa@users.noreply.github.com>
2024-04-13 23:22:31 +0200

    Krejt file-at

---
+++ ProjektiLabCourse1/src/main/java/org/labcourse/digitalschool1/Digitalschool1Application.java
@@ -0,0 +1,13 @@
+package org.labcourse.digitalschool1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Digitalschool1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Digitalschool1Application.class, args);
+	}
+
+}
---
+++ ProjektiLabCourse1/src/test/java/org/labcourse/digitalschool1/Digitalschool1ApplicationTests.java
@@ -0,0 +1,14 @@
+package org.labcourse.digitalschool1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Digitalschool1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+// dsdsd
