d06bf4611c08c5049fe74af576f94e4684236617
fionaisaa <157630752+fionaisaa@users.noreply.github.com>
2024-04-21 23:40:56 +0200

    me e kthy listen e studentave(service)

--- src/main/java/com/example/digitalSchool/student/StudentService.java
+++ src/main/java/com/example/digitalSchool/student/StudentService.java
@@ -1,10 +1,13 @@
 package com.example.digitalSchool.student;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface StudentService {
 
     StudentEntity findById(Long id);
     StudentEntity save(StudentEntity studentEntity);
      List<StudentEntity> findAll();
+    Page<StudentEntity> findAll(Pageable pageable);
 }
