33853895d792a6b7029a29ec1d5a3e8c79c5d2a9
fionaisaa <157630752+fionaisaa@users.noreply.github.com>
2024-04-21 14:38:04 +0200

    me i bo return listen e studentave

--- StudentService.java
+++ StudentService.java
@@ -1,8 +1,12 @@
 package org.labcourse.digitalschool1.Students;
 
+import java.util.List;
+
 public interface StudentService {
 
     StudentEntity findById(Long id);
     StudentEntity save(StudentEntity studentEntity);
+     List<StudentEntity> findAll();
+}
 
 }
